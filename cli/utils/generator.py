f1cbc92db227cc6696efcdf476c1ec8adf7e8f47
Gavin Williams <gavin.williams@molofinance.com>
2024-09-19 14:19:04 +0100

    Support bypassing the OKTA auth using `BYPASS_HEADERS` config
    This commit allows bypassing the OKTA authentication if the request
    contains a header and value which matches that contained within the
    `BYPASS_HEADERS` config map.

--- cli/utils/generator.py
+++ cli/utils/generator.py
@@ -3,27 +3,29 @@ import base64
 
 configuration = {
     "AUTH_REQUEST": {
-	    "client_id": "$CLIENT_ID_FROM_IDP",
-	    "response_type": "code",
-	    "scope": "openid email profile",
-	    "redirect_uri": "https://$CLOUDFRONT_DIST_URL/_callback"
-	},
-	"TOKEN_REQUEST": {
-		"client_id": "$CLIENT_ID_FROM_IDP",
-		"redirect_uri": "https://$CLOUDFRONT_DIST_URL/_callback",
-		"grant_type": "authorization_code",
-		"client_secret": "$CLIENT_SECRET_FROM_IDP"
-	},
-	"DISTRIBUTION": "amazon-oai",
-	"AUTHN": "$IDP_NAME",
-	"PRIVATE_KEY": "$PRIVATE_KEY_GOES_HERE",
-	"PUBLIC_KEY": "$PUBLIC_KEY_GOES_HERE",
-	"DISCOVERY_DOCUMENT": "https://$IDP_DOMAIN_NAME/.well-known/openid-configuration",
-	"SESSION_DURATION": 300,
-	"BASE_URL": "https://$IDP_DOMAIN_NAME/",
-	"CALLBACK_PATH": "/_callback",
-	"AUTHZ": "$IDP_NAME"
+        "client_id": "$CLIENT_ID_FROM_IDP",
+        "response_type": "code",
+        "scope": "openid email profile",
+        "redirect_uri": "https://$CLOUDFRONT_DIST_URL/_callback",
+    },
+    "TOKEN_REQUEST": {
+        "client_id": "$CLIENT_ID_FROM_IDP",
+        "redirect_uri": "https://$CLOUDFRONT_DIST_URL/_callback",
+        "grant_type": "authorization_code",
+        "client_secret": "$CLIENT_SECRET_FROM_IDP",
+    },
+    "BYPASS_HEADERS": {},
+    "DISTRIBUTION": "amazon-oai",
+    "AUTHN": "$IDP_NAME",
+    "PRIVATE_KEY": "$PRIVATE_KEY_GOES_HERE",
+    "PUBLIC_KEY": "$PUBLIC_KEY_GOES_HERE",
+    "DISCOVERY_DOCUMENT": "https://$IDP_DOMAIN_NAME/.well-known/openid-configuration",
+    "SESSION_DURATION": 300,
+    "BASE_URL": "https://$IDP_DOMAIN_NAME/",
+    "CALLBACK_PATH": "/_callback",
+    "AUTHZ": "$IDP_NAME",
 }
+
 
 def scaffold_base_config_file():
     """
@@ -33,13 +35,9 @@ def scaffold_base_config_file():
         config (file) = a base config file written to disk.
     """
 
-    with open('cloudfront_config.json', 'w') as file:
-        file.write(
-            json.dumps(
-                configuration,
-                indent = 4
-            )
-        )
+    with open("cloudfront_config.json", "w") as file:
+        file.write(json.dumps(configuration, indent=4))
+
 
 def generate_rendered_config_file(
     client_id: str,
@@ -48,7 +46,7 @@ def generate_rendered_config_file(
     idp_domain_name: str,
     openssl_private_key: str,
     openssl_public_key: str,
-    idp_name="idp"
+    idp_name="idp",
 ):
     """
     Generates full configuration file with real values.
@@ -59,49 +57,39 @@ def generate_rendered_config_file(
 
     base_config = {
         "AUTH_REQUEST": {
-	        "client_id": f"{client_id}",
-	        "response_type": "code",
-	        "scope": "openid email profile",
-	        "redirect_uri": f"https://{cloudfront_host}/_callback"
-	    },
-	    "TOKEN_REQUEST": {
-	    	"client_id": f"{client_id}",
-	    	"redirect_uri": f"https://{cloudfront_host}/_callback",
-	    	"grant_type": "authorization_code",
-	    	"client_secret": f"{client_secret}"
-	    },
-	    "DISTRIBUTION": "amazon-oai",
-	    "AUTHN": f"{idp_name}",
-	    "PRIVATE_KEY": f"{openssl_private_key}",
-	    "PUBLIC_KEY": f"{openssl_public_key}",
-	    "DISCOVERY_DOCUMENT": f"https://{idp_domain_name}/.well-known/openid-configuration",
-	    "SESSION_DURATION": 300,
-	    "BASE_URL": f"https://{idp_domain_name}/",
-	    "CALLBACK_PATH": "/_callback",
-	    "AUTHZ": f"{idp_name}"
+            "client_id": f"{client_id}",
+            "response_type": "code",
+            "scope": "openid email profile",
+            "redirect_uri": f"https://{cloudfront_host}/_callback",
+        },
+        "TOKEN_REQUEST": {
+            "client_id": f"{client_id}",
+            "redirect_uri": f"https://{cloudfront_host}/_callback",
+            "grant_type": "authorization_code",
+            "client_secret": f"{client_secret}",
+        },
+        "BYPASS_HEADERS": {},
+        "DISTRIBUTION": "amazon-oai",
+        "AUTHN": f"{idp_name}",
+        "PRIVATE_KEY": f"{openssl_private_key}",
+        "PUBLIC_KEY": f"{openssl_public_key}",
+        "DISCOVERY_DOCUMENT": f"https://{idp_domain_name}/.well-known/openid-configuration",
+        "SESSION_DURATION": 300,
+        "BASE_URL": f"https://{idp_domain_name}/",
+        "CALLBACK_PATH": "/_callback",
+        "AUTHZ": f"{idp_name}",
     }
 
-    with open('cloudfront_config_rendered.json', 'w') as file:
-        file.write(
-            json.dumps(
-                base_config,
-                indent = 4
-            )
-        )
+    with open("cloudfront_config_rendered.json", "w") as file:
+        file.write(json.dumps(base_config, indent=4))
 
-def base_64_encode_config(
-    config_file
-):
-    with open(config_file, 'r') as file:
+
+def base_64_encode_config(config_file):
+    with open(config_file, "r") as file:
         config_contents = file.read()
         config_bytes = config_contents.encode()
         file.close()
-    
-    with open(f"encoded_{config_file}", 'w') as file:
-        encoded_config = base64.b64encode(config_bytes).decode('ASCII')
-        file.write(
-            json.dumps({
-                "config": encoded_config
-            }, indent = 4)
-        )
 
+    with open(f"encoded_{config_file}", "w") as file:
+        encoded_config = base64.b64encode(config_bytes).decode("ASCII")
+        file.write(json.dumps({"config": encoded_config}, indent=4))
--- src/js/auth.js
+++ src/js/auth.js
@@ -27,11 +27,11 @@ let pkceCodeChallenge;
  *
  * @param {Object} event is the event that initiates the handler
  * @param {AWS.Context} ctx is the aws lambda context
- * @param {(Error, any) => undefined} cb is the aws callback to signal completion.  This is used
+ * @param {(Error, any) => undefined} cb is the aws callback to signal completion.	This is used
  * instead of the async method because it has more predictable behavior.
  * @param {object} setDependencies is a function that sets the dependencies  If this is undefined
  * (as it will be in production) the setDependencies function in the module will set the
- * dependencies.  If this value is specified (as it will be in tests) then deps will be
+ * dependencies.	If this value is specified (as it will be in tests) then deps will be
  * overwritten with the specified dependencies.
  */
 exports.handler = async (event, ctx, cb, setDeps = setDependencies) => {
@@ -40,6 +40,7 @@ exports.handler = async (event, ctx, cb, setDeps = setDependencies) => {
 	deps = setDeps(deps);
 	try {
 		await prepareConfigGlobals(event);
+
 		return await authenticate(event);
 	} catch (err) {
 		log.error(err.message, { event: event }, err);
@@ -67,21 +68,53 @@ async function authenticate(evt) {
 	const queryString = QueryString.parse(querystring);
 	log.info(config.CALLBACK_PATH);
 	log.info(request.uri);
+
+	// Allow bypass OKTA login
+	if (shouldBypassAuth(request, headers)) {
+		log.info('bypassing OKTA authentication...')
+		return { request };
+	}
+
 	if (request.uri.startsWith(config.CALLBACK_PATH)) {
-		// log.info('callback from OIDC provider received');
+		log.info('callback from OIDC provider received');
 		if (queryString.error) {
 			return handleInvalidQueryString(queryString);
 		}
+
 		log.info(queryString.code);
 		if (queryString.code === undefined || queryString.code === null) {
 			return getUnauthorizedPayload('No Code Found', '', '');
 		}
+
 		return getNewJwtResponse({ evt, request, queryString, headers });
 	}
+
 	if ('cookie' in headers && 'TOKEN' in Cookie.parse(headers.cookie[0].value)) {
+		log.info('verifying JWT response');
 		return getVerifyJwtResponse(request, headers);
-	} // log.info('redirecting to OIDC provider');
+	}
+
+	log.info('redirecting to OIDC provider');
 	return getOidcRedirectPayload(request, headers);
+}
+
+// shouldBypassAuth checks if the request should bypass authentication
+// based on a defined header being specified
+function shouldBypassAuth(request, headers) {
+	let bypassAuth = false;
+
+	Object.entries(headers).forEach(([headerName, header]) => {
+		log.info(`headerName: ${headerName}, headerValue: ${header[0].value}`);
+		Object.entries(config.BYPASS_HEADERS).forEach(([bypassHeader, bypassHeaderValue]) => {
+			log.info(`bypassHeader: ${bypassHeader}, bypassValue: ${bypassHeaderValue}`)
+			if (bypassHeader === headerName && bypassHeaderValue == header[0].value) {
+				log.info('Found a header that matches bypass config...')
+				bypassAuth = true
+			}
+		})
+	})
+
+	return bypassAuth;
 }
 
 // getVerifyJwtResponse gets the appropriate response for verified Jwt.
@@ -285,15 +318,15 @@ async function setJwks() {
 }
 
 function generatePkceCodeVerifier(size = 43) {
-    return Crypto
-    .randomBytes(size)
-    .toString('hex')
-    .slice(0, size)
+		return Crypto
+		.randomBytes(size)
+		.toString('hex')
+		.slice(0, size)
 }
 
 function generatePkceCodeChallenge(codeVerifier){
-    var hash = Crypto.createHash('sha256').update(codeVerifier).digest();
-    return Base64Url.encode(hash);
+		var hash = Crypto.createHash('sha256').update(codeVerifier).digest();
+		return Base64Url.encode(hash);
 }
 
 // sets PKCE code verifier and code challenge values
@@ -402,19 +435,19 @@ function getOidcRedirectPayload(request) {
 // getUnauthorizedPayload generates an appropriate unauthorized response.
 function getUnauthorizedPayload(error, errorDescription, errorUri) {
 	const body = `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <!-- Simple HttpErrorPages | MIT License | https://github.com/AndiDittrich/HttpErrorPages -->
-      <meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>We've got some trouble | 401 - Unauthorized</title>
-      <style type="text/css">/*! normalize.css v5.0.0 | MIT License | github.com/necolas/normalize.css */html{font-family:sans-serif;line-height:1.15;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}article,aside,footer,header,nav,section{display:block}h1{font-size:2em;margin:.67em 0}figcaption,figure,main{display:block}figure{margin:1em 40px}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent;-webkit-text-decoration-skip:objects}a:active,a:hover{outline-width:0}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:inherit}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}dfn{font-style:italic}mark{background-color:#ff0;color:#000}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}audio,video{display:inline-block}audio:not([controls]){display:none;height:0}img{border-style:none}svg:not(:root){overflow:hidden}button,input,optgroup,select,textarea{font-family:sans-serif;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}[type=reset],[type=submit],button,html [type=button]{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{border:1px solid silver;margin:0 2px;padding:.35em .625em .75em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{display:inline-block;vertical-align:baseline}textarea{overflow:auto}[type=checkbox],[type=radio]{box-sizing:border-box;padding:0}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-cancel-button,[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details,menu{display:block}summary{display:list-item}canvas{display:inline-block}template{display:none}[hidden]{display:none}/*! Simple HttpErrorPages | MIT X11 License | https://github.com/AndiDittrich/HttpErrorPages */body,html{width:100%;height:100%;background-color:#21232a}body{color:#fff;text-align:center;text-shadow:0 2px 4px rgba(0,0,0,.5);padding:0;min-height:100%;-webkit-box-shadow:inset 0 0 100px rgba(0,0,0,.8);box-shadow:inset 0 0 100px rgba(0,0,0,.8);display:table;font-family:"Open Sans",Arial,sans-serif}h1{font-family:inherit;font-weight:500;line-height:1.1;color:inherit;font-size:36px}h1 small{font-size:68%;font-weight:400;line-height:1;color:#777}a{text-decoration:none;color:#fff;font-size:inherit;border-bottom:dotted 1px #707070}.lead{color:silver;font-size:21px;line-height:1.4}.cover{display:table-cell;vertical-align:middle;padding:0 20px}footer{position:fixed;width:100%;height:40px;left:0;bottom:0;color:#a0a0a0;font-size:14px}</style>
-  </head>
-  <body>
-      <div class="cover"><h1>Unauthorized</h1><small>Error 401</small><p class="lead">Unauthorized</p><p>Unauthorized</p></div>
-      <footer><p><a href="https://github.com/aws-samples/lambdaedge-openidconnect-samples">cloudfront-auth</a></p></footer>
-  </body>
-  </html>
-  `;
+	<html lang="en">
+	<head>
+		<!-- Simple HttpErrorPages | MIT License | https://github.com/AndiDittrich/HttpErrorPages -->
+		<meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title>We've got some trouble | 401 - Unauthorized</title>
+		<style type="text/css">/*! normalize.css v5.0.0 | MIT License | github.com/necolas/normalize.css */html{font-family:sans-serif;line-height:1.15;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}article,aside,footer,header,nav,section{display:block}h1{font-size:2em;margin:.67em 0}figcaption,figure,main{display:block}figure{margin:1em 40px}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent;-webkit-text-decoration-skip:objects}a:active,a:hover{outline-width:0}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:inherit}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}dfn{font-style:italic}mark{background-color:#ff0;color:#000}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}audio,video{display:inline-block}audio:not([controls]){display:none;height:0}img{border-style:none}svg:not(:root){overflow:hidden}button,input,optgroup,select,textarea{font-family:sans-serif;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}[type=reset],[type=submit],button,html [type=button]{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{border:1px solid silver;margin:0 2px;padding:.35em .625em .75em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{display:inline-block;vertical-align:baseline}textarea{overflow:auto}[type=checkbox],[type=radio]{box-sizing:border-box;padding:0}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-cancel-button,[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details,menu{display:block}summary{display:list-item}canvas{display:inline-block}template{display:none}[hidden]{display:none}/*! Simple HttpErrorPages | MIT X11 License | https://github.com/AndiDittrich/HttpErrorPages */body,html{width:100%;height:100%;background-color:#21232a}body{color:#fff;text-align:center;text-shadow:0 2px 4px rgba(0,0,0,.5);padding:0;min-height:100%;-webkit-box-shadow:inset 0 0 100px rgba(0,0,0,.8);box-shadow:inset 0 0 100px rgba(0,0,0,.8);display:table;font-family:"Open Sans",Arial,sans-serif}h1{font-family:inherit;font-weight:500;line-height:1.1;color:inherit;font-size:36px}h1 small{font-size:68%;font-weight:400;line-height:1;color:#777}a{text-decoration:none;color:#fff;font-size:inherit;border-bottom:dotted 1px #707070}.lead{color:silver;font-size:21px;line-height:1.4}.cover{display:table-cell;vertical-align:middle;padding:0 20px}footer{position:fixed;width:100%;height:40px;left:0;bottom:0;color:#a0a0a0;font-size:14px}</style>
+	</head>
+	<body>
+		<div class="cover"><h1>Unauthorized</h1><small>Error 401</small><p class="lead">Unauthorized</p><p>Unauthorized</p></div>
+		<footer><p><a href="https://github.com/aws-samples/lambdaedge-openidconnect-samples">cloudfront-auth</a></p></footer>
+	</body>
+	</html>
+	`;
 
 	return {
 		body,
@@ -444,18 +477,18 @@ function getUnauthorizedPayload(error, errorDescription, errorUri) {
 // getInternalServerErrorPayload returns an appropriate InternalServerError response.
 function getInternalServerErrorPayload() {
 	const body = `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <!-- Simple HttpErrorPages | MIT License | https://github.com/AndiDittrich/HttpErrorPages -->
-      <meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>We've got some trouble | 500 - Internal Server Error</title>
-      <style type="text/css">/*! normalize.css v5.0.0 | MIT License | github.com/necolas/normalize.css */html{font-family:sans-serif;line-height:1.15;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}article,aside,footer,header,nav,section{display:block}h1{font-size:2em;margin:.67em 0}figcaption,figure,main{display:block}figure{margin:1em 40px}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent;-webkit-text-decoration-skip:objects}a:active,a:hover{outline-width:0}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:inherit}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}dfn{font-style:italic}mark{background-color:#ff0;color:#000}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}audio,video{display:inline-block}audio:not([controls]){display:none;height:0}img{border-style:none}svg:not(:root){overflow:hidden}button,input,optgroup,select,textarea{font-family:sans-serif;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}[type=reset],[type=submit],button,html [type=button]{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{border:1px solid silver;margin:0 2px;padding:.35em .625em .75em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{display:inline-block;vertical-align:baseline}textarea{overflow:auto}[type=checkbox],[type=radio]{box-sizing:border-box;padding:0}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-cancel-button,[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details,menu{display:block}summary{display:list-item}canvas{display:inline-block}template{display:none}[hidden]{display:none}/*! Simple HttpErrorPages | MIT X11 License | https://github.com/AndiDittrich/HttpErrorPages */body,html{width:100%;height:100%;background-color:#21232a}body{color:#fff;text-align:center;text-shadow:0 2px 4px rgba(0,0,0,.5);padding:0;min-height:100%;-webkit-box-shadow:inset 0 0 100px rgba(0,0,0,.8);box-shadow:inset 0 0 100px rgba(0,0,0,.8);display:table;font-family:"Open Sans",Arial,sans-serif}h1{font-family:inherit;font-weight:500;line-height:1.1;color:inherit;font-size:36px}h1 small{font-size:68%;font-weight:400;line-height:1;color:#777}a{text-decoration:none;color:#fff;font-size:inherit;border-bottom:dotted 1px #707070}.lead{color:silver;font-size:21px;line-height:1.4}.cover{display:table-cell;vertical-align:middle;padding:0 20px}footer{position:fixed;width:100%;height:40px;left:0;bottom:0;color:#a0a0a0;font-size:14px}</style>
-  </head>
-  <body>
-      <div class="cover"><h1>Internal Server Error <small>Error 500</small></h1></div>
-  </body>
-  </html>
-  `;
+	<html lang="en">
+	<head>
+		<!-- Simple HttpErrorPages | MIT License | https://github.com/AndiDittrich/HttpErrorPages -->
+		<meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title>We've got some trouble | 500 - Internal Server Error</title>
+		<style type="text/css">/*! normalize.css v5.0.0 | MIT License | github.com/necolas/normalize.css */html{font-family:sans-serif;line-height:1.15;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}article,aside,footer,header,nav,section{display:block}h1{font-size:2em;margin:.67em 0}figcaption,figure,main{display:block}figure{margin:1em 40px}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent;-webkit-text-decoration-skip:objects}a:active,a:hover{outline-width:0}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:inherit}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}dfn{font-style:italic}mark{background-color:#ff0;color:#000}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}audio,video{display:inline-block}audio:not([controls]){display:none;height:0}img{border-style:none}svg:not(:root){overflow:hidden}button,input,optgroup,select,textarea{font-family:sans-serif;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}[type=reset],[type=submit],button,html [type=button]{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{border:1px solid silver;margin:0 2px;padding:.35em .625em .75em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{display:inline-block;vertical-align:baseline}textarea{overflow:auto}[type=checkbox],[type=radio]{box-sizing:border-box;padding:0}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-cancel-button,[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details,menu{display:block}summary{display:list-item}canvas{display:inline-block}template{display:none}[hidden]{display:none}/*! Simple HttpErrorPages | MIT X11 License | https://github.com/AndiDittrich/HttpErrorPages */body,html{width:100%;height:100%;background-color:#21232a}body{color:#fff;text-align:center;text-shadow:0 2px 4px rgba(0,0,0,.5);padding:0;min-height:100%;-webkit-box-shadow:inset 0 0 100px rgba(0,0,0,.8);box-shadow:inset 0 0 100px rgba(0,0,0,.8);display:table;font-family:"Open Sans",Arial,sans-serif}h1{font-family:inherit;font-weight:500;line-height:1.1;color:inherit;font-size:36px}h1 small{font-size:68%;font-weight:400;line-height:1;color:#777}a{text-decoration:none;color:#fff;font-size:inherit;border-bottom:dotted 1px #707070}.lead{color:silver;font-size:21px;line-height:1.4}.cover{display:table-cell;vertical-align:middle;padding:0 20px}footer{position:fixed;width:100%;height:40px;left:0;bottom:0;color:#a0a0a0;font-size:14px}</style>
+	</head>
+	<body>
+		<div class="cover"><h1>Internal Server Error <small>Error 500</small></h1></div>
+	</body>
+	</html>
+	`;
 
 	return { status: '500', statusDescription: 'Internal Server Error', body };
 }
